de178a7299367547d1a729f63a1fc06161941d57
xdc <1434412784@qq.com>
2020-04-01 18:43:25 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: "[name].[contenthash].js"
+        filename: "index.[contenthash].js"
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: "Webpack App",
+        template: "src/assets/index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 };
