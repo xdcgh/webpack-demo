638b3ba6ae51e93a68ee63d070eb214979a2b284
xdc <1434412784@qq.com>
2020-04-01 22:59:48 +0800

    完成懒加载并准备上传

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import x from './x'
+import png from './assets/1.png'
 
-console.log(x);
-console.log('hi');
+const div = document.getElementById('app')
+
+div.innerHTML = `
+    <img src="${png}">
+`
+
+const button = document.createElement('button');
+
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy')
+    promise.then((module) => {
+        const fn = module.default;
+        fn();
+    }, () => {
+    })
+}
+
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,38 @@ module.exports = {
             title: "Webpack App",
             template: "src/assets/index.html"
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: ['style-loader', 'css-loader', 'stylus-loader'],
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    }
+                ],
+            },
+        ],
+    },
 };
